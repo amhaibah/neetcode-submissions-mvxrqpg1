1691f335c84927c219a9f8d5416189315a1fbc5a
amhaibah <113548420+amhaibah@users.noreply.github.com>
2026-04-20 00:08:56 -0500

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #growing hashmap
+        hash_map = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash_map:
+                return [hash_map[target - nums[i]], i]
+            else:
+                hash_map[nums[i]] = i
+        return []
+
+
+
+        
+        
